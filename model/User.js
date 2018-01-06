f6aa00ded7bc3e81a35bee9f018aff2c42d6e5b2
Eddie-CooRo <1300100eddiecooro@gmail.com>
2018-01-06 15:13:53 +0330

    Adding validation and bcrypt support to User model

--- model/User.js
+++ model/User.js
@@ -1,11 +1,22 @@
 var mongoose = require("mongoose");
+const mb = require('mongoose-bcrypt');
 var Schema = mongoose.Schema;
 
+function validateEmail(email) {
+    var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailRegex.test(email.text);
+ }
+
 var UserSchema = new Schema({
-    isValid: {type:Boolean,default:false},
-    username: {type: String,unique:true},
-    password:String,
-    email:{type:String,unique:true},
+    emailValidated: {type:Boolean,default:false},
+    username: {type: String,required:true,unique:true},
+    password: {type: String,required:true, bcrypt: true},
+    email:{
+        type:String,
+        unique: true,
+        required: true,
+        validate: validateEmail,
+    },
     avatar:String,
     nickname:String,
     gender:String,
@@ -16,13 +27,18 @@ var UserSchema = new Schema({
         trackCount:{type:Number,min:1},
         shuffle:{type:Boolean,default:false},
         tracks:[Number],
-
     }],
     followedArtist:[Number],
     songsRel:[{songId:Number,rel:String}],
     loginTime:[Number]
+});
 
+UserSchema.pre('save', function(next){
+    this.followedArtist = this.followedArtist.filter(function(item, pos) {
+        return this.followedArtist.indexOf(item) == pos;
+    })
+});
 
+UserSchema.plugin(require('mongooseBcrypt'), {rounds: 10});
 
-
-})
+module.exports = mongoose.model("User", UserSchema);
