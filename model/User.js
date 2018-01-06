0ddce33a3d9f0564e5c5d54c862cf56c68d0325f
Mohamamd Sharifi <techshow78@gmail.com>
2018-01-06 14:18:03 +0330

    PlayList Added

--- model/User.js
+++ model/User.js
@@ -10,6 +10,14 @@ var UserSchema = new Schema({
     nickname:String,
     gender:String,
     age:{type:Number,min:1,max:200},
+    playlist:[{
+        name:String,
+        cover:String,
+        trackCount:{type:Number,min:1},
+        shuffle:{type:Boolean,default:false},
+        tracks:[Number],
+
+    }],
     followedArtist:[Number],
     songsRel:[{songId:Number,rel:String}],
     loginTime:[Number]
