fe20ad4e17e1d36e66f3f5231b52ca30f3f416a1
Eddie-CooRo <1300100eddiecooro@gmail.com>
2018-01-06 14:20:44 +0330

    Artist & Album Models

---
+++ models/Artist.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+
+var SongSchema = new mongoose.Schema({
+    name: String,
+    desc: String,
+    genre: [String],
+    label: [String],
+    lyrics: [String],
+    releaseData: Date,
+    url: String,
+    cover: String,
+    album: { type: Number, default: -1 },
+});
+
+var ArtistSchema = new mongoose.Schema({
+    name: String,
+    desc: String,
+    songs: [{
+        rel: String, // For example: Singer | Writter | Composer ...        
+        song: SongSchema
+    }]
+}, {
+    timestamps: true
+});
+
+
+module.exports = mongoose.model("Artist", ArtistSchema);
