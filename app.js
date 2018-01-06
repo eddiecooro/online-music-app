fa9fd987e7413aa6ced602a0e2700d6dfbe479b6
Eddie-CooRo <1300100eddiecooro@gmail.com>
2018-01-06 15:34:15 +0330

    adding helmet security middleware

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+// adding helmet secury middleware
+const helmet = require('helmet');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,6 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(helmet);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
