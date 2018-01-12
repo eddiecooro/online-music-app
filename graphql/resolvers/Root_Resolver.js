e22b45c982c55d1d8f4f16933e0c752705c5b7f4
Eddie-CooRo <1300100eddiecooro@gmail.com>
2018-01-13 00:30:56 +0330

    viewer and search resolvers added to rootResolvers

--- graphql/resolvers/Root_Resolver.js
+++ graphql/resolvers/Root_Resolver.js
@@ -9,6 +9,22 @@ module.exports = {
             }).catch((err)=>{
                 console.log(err);
             });
+        },
+        search: (source, args, context)=>{
+            let modelsToSearch = ["User","Playlist","Song","Artist","Album"];
+            return db.search(modelsToSearch,args.text).then((data)=>{
+                return data;
+            }).catch((err)=>{
+                console.log(err);
+            });
+        },
+        viewer: (source,args,context)=>{
+            if(context.user){
+                context.user.__modelName = "User";
+                return context.user;
+            } else {
+                return null
+            }
         }
     },
     //Specify Which Resolve Should Go
